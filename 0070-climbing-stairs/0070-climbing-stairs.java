2f1f4a3e304b08264994a874340e8a9d2354222f
Siddharth Pandey <officialsiddharth.pandey@gmail.com>
2026-07-30 17:05:18 +0530

    Time: 0 ms (100%), Space: 42.2 MB (36.25%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n == 1 || n == 2){
+            return n;
+        }
+        int n1 = 1;
+        int n2 = 2;
+
+        for(int i=3; i<=n; i++){
+            int temp = n1 + n2;
+            n1 = n2;
+            n2 = temp;
+        }
+        return n2;
+    }
+}
