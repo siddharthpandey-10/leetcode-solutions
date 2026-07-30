5c62fb3c9a6e721a1bed5a986c575c962753f7f8
Siddharth Pandey <officialsiddharth.pandey@gmail.com>
2026-07-30 16:55:19 +0530

    Time: 0 ms (100%), Space: 42.3 MB (15.87%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int fib(int n) {
+       if(n == 0 || n == 1){
+        return n;
+       }
+
+       int prev0 = 0;
+       int prev1 = 1;
+
+       for(int i=2; i<=n; i++){
+        int temp = prev0 + prev1;
+        prev0 = prev1;
+        prev1 = temp;
+       } 
+       return prev1;
+    }
+}
