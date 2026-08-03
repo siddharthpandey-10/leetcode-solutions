9bf3712f560134bd8c9cce1832ca7e5b1fbc88fb
Siddharth Pandey <officialsiddharth.pandey@gmail.com>
2026-08-03 16:50:25 +0530

    Time: 24 ms (94.54%), Space: 44.5 MB (8.09%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.java
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if(node == null){
+            return null;
+        }
+        HashMap<Node, Node> map = new HashMap<>();
+        return clone(node, map);
+    }
+
+    private Node clone(Node node, HashMap<Node, Node> map){
+        Node newNode = new Node(node.val);
+        map.put(node, newNode);
+
+        for(Node neighbor : node.neighbors){
+            if(!map.containsKey(neighbor)){
+                newNode.neighbors.add(clone(neighbor, map));
+            }
+            else{
+                newNode.neighbors.add(map.get(neighbor));
+            }
+        }
+        return newNode;
+    }
+}
