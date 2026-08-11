a115d6ec5878fc537701fbbb078a4d2094797e5f
Siddharth Pandey <officialsiddharth.pandey@gmail.com>
2026-08-12 05:28:20 +0530

    Time: 0 ms (100%), Space: 47.6 MB (81.96%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int trap(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+
+        int leftMax = height[left];
+        int  rightMax = height[right];
+
+        int total = 0;
+        while(left < right){
+            if(leftMax < rightMax){
+                left ++;
+                leftMax = Math.max(leftMax, height[left]);
+                total += leftMax - height[left];
+            }
+
+            else{
+                right--;
+                rightMax = Math.max(rightMax, height[right]);
+                total += rightMax - height[right];
+            }
+        }
+        return total;
+    }
+}
