bbc2c2fd2ee6c10ec8e1bcd8ef0de0ea0c3317e9
Siddharth Pandey <officialsiddharth.pandey@gmail.com>
2026-08-15 16:42:54 +0530

    Time: 2 ms (81.93%), Space: 46.5 MB (29.38%) - LeetHub

---
+++ 3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int n = fruits.length;
+        int used = 0;
+        boolean[] visited = new boolean[n];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(fruits[i] <= baskets[j]){
+                    if(!visited[j]){
+                        visited[j] = true;
+                        used++;
+                        break;
+                    }
+                }
+            }
+        }
+        int unused = n-used;
+        return used == n ? 0 : unused;
+    }
+}
